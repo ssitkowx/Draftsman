438615629abfa13016a3aa8735cfc439fe70dceb
sylsit <sitkowskis@interia.pl>
2020-08-08 21:03:11 +0200

    DrawRect display logic added

--- Project/Display.cpp
+++ Project/Display.cpp
@@ -2,7 +2,57 @@
 //////////////////////////////// INCLUDES /////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
+#include "Utils.h"
+#include <memory.h>
 #include "Display.h"
+
+///////////////////////////////////////////////////////////////////////////////
+//////////////////////////////// FUNCTIONS ////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+
+bool Display::DrawRect (const uint16_t v_xPos, const uint16_t v_yPos, const uint16_t v_width, const uint16_t v_length, const Display::EColors eColor)
+{
+    if (validateRect (v_xPos, v_yPos, v_width, v_length) == false) { return false; }
+
+    const uint16_t rectLen  = Config.Width * Config.MaxLinesPerTransfer;
+    uint16_t rect [rectLen] = { };
+    memset (rect, getColor (eColor), rectLen * sizeof (uint16_t));
+    uint8_t maxRects = calculateRects (v_length);
+
+    if (maxRects == ONE) { sendLines (v_xPos, v_yPos, v_width, v_length, static_cast <uint16_t *> (rect)); }
+    else
+    {
+        uint16_t yPos   = v_yPos;
+        uint16_t length = ZERO;
+        for (uint8_t rectNum = ONE; rectNum <= maxRects; rectNum++)
+        {
+            if (rectNum == maxRects) { length = v_length + v_yPos - yPos; }
+            else
+            {
+                length = Config.MaxLinesPerTransfer;
+                yPos   = yPos + length;
+            }
+
+            sendLines (v_xPos, yPos, v_width, length, static_cast <uint16_t *> (rect));
+        }
+    }
+
+    return true;
+}
+
+bool Display::validateRect (const uint16_t v_xPos, const uint16_t v_yPos, const uint16_t v_width, const uint16_t v_length)
+{
+    return (((v_xPos + v_width)  > Config.Width)  ||
+            ((v_yPos + v_length) > Config.Length)) ? false : true;
+}
+
+uint8_t Display::calculateRects (const uint16_t v_length)
+{
+    double  rects    = v_length / Config.MaxLinesPerTransfer;
+    uint8_t maxRects = static_cast <uint8_t> (rects);
+
+    return ((v_length % Config.MaxLinesPerTransfer) != ZERO) ? ++maxRects : maxRects;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 /////////////////////////////// END OF FILE ///////////////////////////////////
--- Project/Display.h
+++ Project/Display.h
@@ -35,12 +35,30 @@ class Display
             eNavy
         };
 
-        Display          () = default;
+        struct Configuration
+        {
+            uint16_t Length;
+            uint16_t Width;
+            uint16_t MaxLinesPerTransfer;
+            uint8_t  RectsNumber;
+        };
+
+        const Configuration Config;
+
+        Display (const Configuration v_config) : Config (v_config) {}
         virtual ~Display () = default;
 
-        virtual void DrawRect    (const uint16_t v_xPos, const uint16_t v_yPos, const uint16_t v_width, const uint16_t v_length, const EColors eColor) = 0;
-        virtual void DrawText    (const uint16_t v_xPos, const uint16_t v_yPos, const uint8_t & data  , const uint16_t v_len   , const EColors eColor) = 0;
-        virtual void DrawPicture (const uint16_t v_xPos, const uint16_t v_yPos, const uint8_t & data  , const uint16_t v_len)                          = 0;
+        bool            DrawRect       (const uint16_t v_xPos, const uint16_t v_yPos, const uint16_t v_width, const uint16_t v_length, const EColors eColor         );
+        virtual bool    DrawText       (const uint16_t v_xPos, const uint16_t v_yPos, const uint8_t & data  , const uint16_t v_len   , const EColors eColor         ) = 0;
+        virtual bool    DrawPicture    (const uint16_t v_xPos, const uint16_t v_yPos, const uint8_t & data  , const uint16_t v_len                                  ) = 0;
+
+    protected:
+        virtual void    sendLines      (const uint16_t v_xPos, const uint16_t v_yPos, const uint16_t v_width, const uint16_t v_length, const uint16_t * const v_data) = 0;
+        virtual uint8_t getColor       (const Display::EColors eColor                                                                                               ) = 0;
+
+    private:
+        bool            validateRect   (const uint16_t v_xPos, const uint16_t v_yPos, const uint16_t v_width, const uint16_t v_length);
+        uint8_t         calculateRects (const uint16_t v_length);
 };
 
 ///////////////////////////////////////////////////////////////////////////////
