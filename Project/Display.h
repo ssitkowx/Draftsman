35957ebe561f51393c67e4ea8ab1d30aba7d5410
sylsit <sitkowskis@interia.pl>
2020-12-12 15:25:08 +0100

    optimization with compiler explorer

--- Project/Display.h
+++ Project/Display.h
@@ -87,13 +87,13 @@ class Display
         const Config_t config;
         ~Display () = default;
 
-        virtual bool validate (const Rectangle & v_rect)
+        constexpr bool validate (const Rectangle & v_rect)
         {
             return (((v_rect.Coordinate.X + v_rect.Dimension.Width) >= config.Dimension.Width) ||
                     ((v_rect.Coordinate.Y + v_rect.Dimension.Height) >= config.Dimension.Height)) ? false : true;
         }
 
-        uint8_t calculate (const Rectangle::Dimensions & v_dimensions)
+        constexpr uint8_t calculate (const Rectangle::Dimensions & v_dimensions)
         {
             double rects         = (v_dimensions.Width * v_dimensions.Height) / (config.Dimension.Width * config.LinesPerTransfer);
             double aditionalRect = (v_dimensions.Width * v_dimensions.Height) % (config.Dimension.Width * config.LinesPerTransfer);
